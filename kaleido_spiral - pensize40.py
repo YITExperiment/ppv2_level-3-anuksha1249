f343e88fabfe7cbe3d13cd75f34134fcac48a6f0
anuksha1249 <135410635+anuksha1249@users.noreply.github.com>
2023-07-08 20:16:41 +0530

    Add files via upload

--- kaleido_spiral - pensize40.py
+++ kaleido_spiral - pensize40.py
@@ -6,11 +6,11 @@ colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 def draw_circle(size):
     turtle.pencolor(next(colors))
     turtle.circle(size)
-    draw_circle(size+5)
+    draw_circle(size+1)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(4)
+turtle.pensize(40)
 draw_circle(30)
 
 
